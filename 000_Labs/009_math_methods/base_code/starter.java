65427fd8d896a71a352f4c22584951d1f077b431
Jadenz2010 <sqrlygrlz@gmail.com>
2026-09-14 21:05:33 +0000

    update

--- 000_Labs/009_math_methods/base_code/starter.java
+++ 000_Labs/009_math_methods/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Jaden Zheng
+ *  Date: 9/10/26
 */
 
 import java.util.Scanner;
@@ -9,6 +9,13 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		Math.max (13 - 6 * 11, 30 % 7 * (-2)) ;
+		System.out.println("Max: " + Math.max (13 - 6 * 11, 30 % 7 * (-2))) ;
+		System.out.println("Sqrt: "+ Math.sqrt (3 * 8 + 31 % 7));
+		System.out.println("Pow: " + Math.pow (37 / 3, 35 % 21));
+		System.out.println("Max :" + Math.max( Math.pow( 2,14%3) , Math.sqrt ( 2*6) ));
+		
+	
+		
+
 	}
 }
